e0730785d84a6473c0e71032e522dc0f2c24cfc0
ei-show <offiter.ei.show@gmail.com>
2020-10-16 00:05:11 +0900

    pages/index.jsのjsx修正中

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/reset.scss'
 import '../styles/global.scss'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHeadset, faEdit, faCode } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faHeadset, faEdit, faCode);
+
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
 }
